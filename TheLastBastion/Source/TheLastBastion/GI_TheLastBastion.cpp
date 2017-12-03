05f4e2d808d0fe0145b48945f088deadb4319934
gesong2017 <GSong@fullsail.edu>
2017-12-03 15:22:09 -0500

    Game Instance C++ Script Created

---
+++ TheLastBastion/Source/TheLastBastion/GI_TheLastBastion.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GI_TheLastBastion.h"
+
+#include "UObject/ConstructorHelpers.h"
+
+
+
+
---
+++ TheLastBastion/Source/TheLastBastion/GI_TheLastBastion.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GI_TheLastBastion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELASTBASTION_API UGI_TheLastBastion : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	// LAN Mode Functions
+
+
+	//
+	
+private:
+
+};
