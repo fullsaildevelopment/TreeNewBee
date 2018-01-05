ad6aed454fe19ee7d81e23dd52739363729ab3a8
Shiwei Du <dushiweic2@hotmail.com>
2018-01-05 11:52:59 -0500

    DamageType

---
+++ TheLastBastion/Source/TheLastBastion/Combat/TLBDamageType.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "TLBDamageType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELASTBASTION_API UTLBDamageType : public UDamageType
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
