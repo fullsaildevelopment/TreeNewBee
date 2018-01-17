5c6e391ecc7bc5581d9fa42e4dcf4921cc8cf500
DESKTOP-76F9ME7\Rorschach <gesong2017@gmail.com>
2018-01-17 18:42:54 -0500

    Range Enemy Bullet Velocity problem fixed

--- TheLastBastion/Source/TheLastBastion/Combat/RangeWeapon.cpp
+++ TheLastBastion/Source/TheLastBastion/Combat/RangeWeapon.cpp
@@ -3,8 +3,11 @@
 #include "RangeWeapon.h"
 #include "Components/StaticMeshComponent.h"
 #include "AICharacters/TheLastBastionEnemyCharacter.h"
+#include "AI/TheLastBastionBaseAIController.h"
 #include "Projectile.h"
 #include "DrawDebugHelpers.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyAllTypes.h"
 
 #define ECC_EnemyBody ECollisionChannel::ECC_GameTraceChannel3
 #define ECC_HeroBody  ECollisionChannel::ECC_GameTraceChannel1
@@ -37,48 +40,102 @@ void ARangeWeapon::Fire()
 	// Trace the world from pawn eyes to crosshair location
 	if (GearOwner != nullptr && ProjectileClassBP != nullptr)
 	{   
-		// Find Player eyes location and rotation
-		FVector EyesLocation;
-		FRotator EyesRotation;
-		GearOwner->GetActorEyesViewPoint(EyesLocation, EyesRotation);
+		ATheLastBastionEnemyCharacter* RangeEnemyCharacter= Cast<ATheLastBastionEnemyCharacter>(GearOwner);
 
-		// Find the impact location
-		FVector ShotDirection = EyesRotation.Vector();
-		FVector TraceEnd = EyesLocation + (ShotDirection * ShootingRange);
+		// Check if the gearowner is a enemy character
+		if (RangeEnemyCharacter)
+		{   
+			// Get the AI Controller from the enemy
+			ATheLastBastionBaseAIController* EnemyAIController = Cast<ATheLastBastionBaseAIController>(RangeEnemyCharacter->GetController());
+			if (EnemyAIController)
+			{   
+				// Get Enemy's target actor location to initialize velocity for projectile
+				UBehaviorTreeComponent* BehaviorTreeComp = EnemyAIController->GetBTComp();
+				if (BehaviorTreeComp)
+				{
+					UBlackboardComponent* BlackboardComp = BehaviorTreeComp->GetBlackboardComponent();
+					if (BlackboardComp)
+					{
+						AActor* TargetActor = Cast<AActor>(BlackboardComp->GetValue<UBlackboardKeyType_Object>(EnemyAIController->GetKeyID_TargetActor()));
+						// Correctly Spawn the projectile
+						FVector MuzzleLocation = MeshComp->GetSocketLocation(MuzzleSocketName);
+						FRotator MuzzleRotation = MeshComp->GetSocketRotation(MuzzleSocketName);
 
-		FCollisionQueryParams QueryParams;
-		QueryParams.AddIgnoredActor(GearOwner);
-		QueryParams.AddIgnoredActor(this);
-		QueryParams.bTraceComplex = true;
-		QueryParams.bReturnPhysicalMaterial = false;
-		FHitResult Hit;
+						// Create and Initialize the spawn parameters for the projectile
+						FActorSpawnParameters SpawnParams;
+						SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+						AProjectile* CrossbowProjectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClassBP, MuzzleLocation, MuzzleRotation, SpawnParams);
 
-		if (GetWorld()->LineTraceSingleByChannel(Hit, EyesLocation, TraceEnd, ECollisionChannel::ECC_Visibility))
-		{
-			// Override TracerEndPoint when bullet get blocked
-			TraceEnd = Hit.ImpactPoint;
+						// Calculate the velocity for the projectile
+						FVector FlyDir = (TargetActor->GetActorLocation() - MuzzleLocation).GetSafeNormal();
+						CrossbowProjectile->GetProjectileMovementComp()->Velocity = FlyDir * BulletSpeed;
+					}
+					else
+					{
+						UE_LOG(LogTemp, Warning, TEXT("Can't get Blackboard Component from BehaviorTree Component"));
+						return;
+					}
+
+				}
+				else
+				{
+					UE_LOG(LogTemp, Warning, TEXT("Can't find Enemy AI BehaviorTreeComp"));
+					return;
+				}
+
+			}
+			else
+			{   
+				UE_LOG(LogTemp, Warning, TEXT("Can't find Enemy AI Controller"));
+				return;
+			}
 		}
 
+		else
+		{
+			// Find Player eyes location and rotation
+			FVector EyesLocation;
+			FRotator EyesRotation;
+			GearOwner->GetActorEyesViewPoint(EyesLocation, EyesRotation);
 
-		// Draw a bebug line when linetrace hits something
-		DrawDebugLine(GetWorld(), EyesLocation, TraceEnd, FColor::Red, false, 2.0f, 0, 1.0f);
+			// Find the impact location
+			FVector ShotDirection = EyesRotation.Vector();
+			FVector TraceEnd = EyesLocation + (ShotDirection * ShootingRange);
 
-		// Correctly Spawn the projectile
-		FVector MuzzleLocation = MeshComp->GetSocketLocation(MuzzleSocketName);
-		FRotator MuzzleRotation = MeshComp->GetSocketRotation(MuzzleSocketName);
+			FCollisionQueryParams QueryParams;
+			QueryParams.AddIgnoredActor(GearOwner);
+			QueryParams.AddIgnoredActor(this);
+			QueryParams.bTraceComplex = true;
+			QueryParams.bReturnPhysicalMaterial = false;
+			FHitResult Hit;
 
-		// Create and Initialize the spawn parameters for the projectile
-		FActorSpawnParameters SpawnParams;
-		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+			if (GetWorld()->LineTraceSingleByChannel(Hit, EyesLocation, TraceEnd, ECollisionChannel::ECC_Visibility))
+			{
+				// Override TracerEndPoint when bullet get blocked
+				TraceEnd = Hit.ImpactPoint;
+			}
 
 
-		AProjectile* CrossbowProjectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClassBP, MuzzleLocation, EyesRotation, SpawnParams);
+			// Draw a bebug line when linetrace hits something
+			DrawDebugLine(GetWorld(), EyesLocation, TraceEnd, FColor::Red, false, 2.0f, 0, 1.0f);
 
-		// Calculate the velocity for the projectile
-		FVector FlyDir = (TraceEnd - MuzzleLocation).GetSafeNormal();
-		CrossbowProjectile->GetProjectileMovementComp()->Velocity = FlyDir * BulletSpeed;
+			// Correctly Spawn the projectile
+			FVector MuzzleLocation = MeshComp->GetSocketLocation(MuzzleSocketName);
+			FRotator MuzzleRotation = MeshComp->GetSocketRotation(MuzzleSocketName);
 
-		// Draw a bebug line from weapon to impact position
-		DrawDebugLine(GetWorld(), MuzzleLocation, TraceEnd, FColor::Green, false, 2.0f, 0, 1.0f);
+			// Create and Initialize the spawn parameters for the projectile
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+
+			AProjectile* CrossbowProjectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClassBP, MuzzleLocation, MuzzleRotation, SpawnParams);
+
+			// Calculate the velocity for the projectile
+			FVector FlyDir = (TraceEnd - MuzzleLocation).GetSafeNormal();
+			CrossbowProjectile->GetProjectileMovementComp()->Velocity = FlyDir * BulletSpeed;
+
+			// Draw a bebug line from weapon to impact position
+			DrawDebugLine(GetWorld(), MuzzleLocation, TraceEnd, FColor::Green, false, 2.0f, 0, 1.0f);
+		}
 	}
 }
