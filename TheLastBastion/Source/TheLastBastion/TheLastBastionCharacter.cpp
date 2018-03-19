ed7a3c2d45ce916df70743745b8942ee24b697d2
DESKTOP-76F9ME7\Rorschach <gesong2017@gmail.com>
2018-03-19 18:41:35 -0400

    Audio Asset Added

--- TheLastBastion/Source/TheLastBastion/TheLastBastionCharacter.cpp
+++ TheLastBastion/Source/TheLastBastion/TheLastBastionCharacter.cpp
@@ -75,6 +75,7 @@ ATheLastBastionCharacter::ATheLastBastionCharacter()
 	AudioComp = CreateDefaultSubobject<UAudioComponent>(TEXT("AUDIOComp"));
 	AudioComp->bAllowSpatialization = false;
 	AudioComp->bAutoActivate = false;
+	AudioComp->bOverrideAttenuation = true;
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 }
